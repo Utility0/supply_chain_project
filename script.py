5f25434c86f148b64873550c6dc45907d756ab33
Utility0 <assuiedjul@cy-tech.fr>
2022-01-20 22:15:27 +0100

    Final Commit

--- script.py
+++ script.py
@@ -7,21 +7,21 @@ g = Graph()
 
 
 # Generate
-g.genGraph(15,3,["time","co2"],0.01)
+g.genGraph(15,3,["time","co2","water"],0.01)
 
 # Store Graph as Json
-g.storeJson("data/Graph.json")
+#g.storeJson("data/Graph.json")
 
 # # Load Graph from Json
 # g.loadJson("Graph.json")
 # print('Loaded')
 
 # Display Graph networkx
-g.showGraph("images/graph.png")
+#g.showGraph("images/graph.png")
 
 # Generate Data
-# data  = g.genData(100)
-# print('Gen')
+#data  = g.genData(100)
+#print(data)
 
 #Store Result As Json
 # def storeJson(path) -> None:
@@ -31,8 +31,12 @@ g.showGraph("images/graph.png")
 # storeJson('data.json')
 
 # Store Result as Neo4J
-#
-#
+q = g.graphToNeo4J("bolt://localhost:7687","neo4j","pswd",False)
+print(q)
+q = g.dataToNeo4J(20,"bolt://localhost:7687","neo4j","pswd",False)
+print(q)
+# print(q[0]["extractId"])
+
 # Todo
 
 # Print Results
